5d94f60c032f17879096c72754c0de6c1a3dcb34
Kevin Guan <github@pigzy.net>
2022-05-05 11:28:12 -0700

    Update pitching_machine.py

--- src/pitching_machine.py
+++ src/pitching_machine.py
@@ -1,4 +1,10 @@
+import random
 print('fixed')
 with open('texts/hello.txt', 'r') as f:
     for line in f:
         print(line.rstrip())
+if random.randint(0, 1) == 0:
+    # runtime error
+    print(x)
+else:
+    print('No Error')

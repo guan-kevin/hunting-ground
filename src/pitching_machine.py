d0c075166ca8ee302e149e302a76fc8bf2f21198
Kevin Guan <github@pigzy.net>
2022-05-05 11:38:33 -0700

    Fixed bug

--- src/pitching_machine.py
+++ src/pitching_machine.py
@@ -3,7 +3,7 @@ print('fixed')
 with open('texts/hello.txt', 'r') as f:
     for line in f:
         print(line.rstrip())
-if random.randint(0, 0) == 0:
+if random.randint(0, 10000) == 0:
     # runtime error
     x = 0
     x = x + 'string'

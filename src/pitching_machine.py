f107373bf259dbbce236411860f6b2fd388cdb46
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-07-08 11:44:41 -0700

    Fail me

--- src/pitching_machine.py
+++ src/pitching_machine.py
@@ -3,10 +3,5 @@ print('fixed')
 with open('texts/hello.txt', 'r') as f:
     for line in f:
         print(line.rstrip())
-if random.randint(0, 10000) == 0:
-    # runtime error
-    x = 0
-    x = x + 'string'
-    print(x)
-else:
-    print('No Error')
+        
+print('HELLO'

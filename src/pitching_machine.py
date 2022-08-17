2bddb58f2fc7198a99fdeef55e015280d8d3c50f
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-08-17 15:04:34 -0700

    Update pitching_machine.py

--- src/pitching_machine.py
+++ src/pitching_machine.py
@@ -2,5 +2,26 @@ import os
 print('HELLO WORLD')
 x = 1
 print(x)
+
 print('ABC:')
 print(os.getenv('ABC'))
+
+print('ABC2:')
+print(os.getenv('ABC2'))
+
+print('ABC3:')
+print(os.getenv('ABC3'))
+
+print('RANDOM:')
+print(os.getenv('RANDOM'))
+
+print('NOT-RANDOM:')
+print(os.getenv('NOT-RANDOM'))
+
+print('XYZ:')
+print(os.getenv('XYZ'))
+
+print('WXYZ:')
+print(os.getenv('WXYZ'))
+
+print(y)

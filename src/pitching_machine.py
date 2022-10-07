5fffcb94bd6145ee08fd4e3f740eb9f6feaa4d51
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-10-07 13:20:17 -0700

    Fixed version 55

--- src/pitching_machine.py
+++ src/pitching_machine.py
@@ -1,7 +1,7 @@
 # import requests
 
-print(Buggy version 55)
-# print('Fixed version 54')
+# print(Buggy version 55)
+# print('Fixed version 55')
 
 # r = requests.get('https://google.com')
 # print(r.status_code)
